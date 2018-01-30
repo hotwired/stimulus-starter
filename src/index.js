92a43a2089782a336d93d949aaabc20e88c28c7d
Javan Makhmali <javan@javan.us>
2018-01-30 15:41:56 -0500

    Update for Stimulus 1.0.0
    🎉

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { Application } from "stimulus"
-import { autoload } from "stimulus/webpack-helpers"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
-
-autoload(context, application)
+application.load(definitionsFromContext(context))
