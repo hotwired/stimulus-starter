809e428698f7f22b0c5e881a91f798e6cfa6f25a
jenish-d <Jenishdobariya66@gmail.com>
2022-04-16 14:33:16 +0530

    Use camelcase

--- src/controllers/hello_controller.js
+++ src/controllers/hello_controller.js
@@ -1,12 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = [ "firstname", "lastname", "sexmale", "sexfemale", "age", "firstnamedisplay", "lastnamedisplay", "agedisplay", "genderdisplay", "formcontainer", "displaycontainer" ]
+    static targets = [ "firstName", "lastname", "sexmale", "sexfemale", "age", "firstnamedisplay", "lastnamedisplay", "agedisplay", "genderdisplay", "formcontainer", "displaycontainer" ]
 
     save() {
         this.displaycontainerTarget.classList.remove("hidden")
         this.sexmaleTarget.checked || this.sexfemaleTarget.checked ? ( this.sexmaleTarget.checked ? this.genderdisplayTarget.innerHTML = "Male" : this.genderdisplayTarget.innerHTML = "Female" ) : this.genderdisplayTarget.innerHTML = "Gender not selected"
-        this.firstnamedisplayTarget.innerHTML = this.firstnameTarget.value
+        this.firstnamedisplayTarget.innerHTML = this.firstNameTarget.value
         this.lastnamedisplayTarget.innerHTML = this.lastnameTarget.value
         this.agedisplayTarget.innerHTML = this.ageTarget.value
         this.formcontainerTarget.classList.add("hidden")
