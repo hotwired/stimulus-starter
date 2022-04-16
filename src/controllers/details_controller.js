2ab5830da6cba2ec5a9e527ca686a5c99a5e273f
jenish-d <Jenishdobariya66@gmail.com>
2022-04-16 22:19:29 +0530

    Add form validation

--- src/controllers/details_controller.js
+++ src/controllers/details_controller.js
@@ -3,23 +3,22 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = [ "firstName", "lastName", "sexMale", "sexFemale", "age", "firstNameDisplay", "lastNameDisplay", "ageDisplay", "genderDisplay", "formContainer", "displayContainer" ]
 
-   
-    getAndDisplayPatientDetails() {
-        // validation of Patient details form
+    validatePatientForm() {
         if(this.firstNameTarget.value == ""){return alert("Please enter first name")}
         if(this.lastNameTarget.value == ""){return alert("Please enter last name")}
         if(!this.sexMaleTarget.checked && !this.sexFemaleTarget.checked){return alert("Please select gender")}
         if(this.ageTarget.value == ""){return alert("Please enter age")}
-
+        this.displayPatientDetails()
+    }
+   
+    displayPatientDetails() {
         // Show display-elements
         this.displayContainerTarget.classList.remove("hidden")
-
         // Get values from input fields and assign it to display elements
         this.sexMaleTarget.checked ? this.genderDisplayTarget.innerHTML = "Male" : this.genderDisplayTarget.innerHTML = "Female" 
-        this.firstNameTarget.value == "" ? console.log("Please enter firstName") : this.firstNameDisplayTarget.innerHTML = this.firstNameTarget.value
+        this.firstNameDisplayTarget.innerHTML = this.firstNameTarget.value
         this.lastNameDisplayTarget.innerHTML = this.lastNameTarget.value
         this.ageDisplayTarget.innerHTML = this.ageTarget.value
-
         // Hide Form
         this.formContainerTarget.classList.add("hidden")
     }
