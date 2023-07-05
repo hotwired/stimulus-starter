91f9f5601fd6ad1a9130e2713ce264ad06d082a7
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2023-07-05 17:31:58 +0700

    Targets Map Important Elements To Controller Properties

--- src/controllers/hello_controller.js
+++ src/controllers/hello_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["name"];
+
   greet() {
-    console.log(`Hello, Stimulus! ${this.element}`);
+    const element = this.nameTarget;
+    const name = element.value;
+
+    console.log(`Hello ${name}!`);
   }
 }
