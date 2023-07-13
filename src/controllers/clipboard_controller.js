e2b207147fe84b539ac6e60c28b682ebcf052078
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2023-07-13 10:32:40 +0700

    Progressively Enhancing the PIN Field

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -1,10 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["source"];
+  static targets = ["source"]
+  // this.sourceTarget
+  // this.sourceTargets
+  // this.hasSourceTarget
+
+  static classes = ["supported"]
+  // this.supportedClass
+
+  connect() {
+    if ("clipboard" in navigator) {
+      this.element.classList.add(this.supportedClass)
+    }
+  }
 
   copy(event) {
-    event.preventDefault();
-    navigator.clipboard.writeText(this.sourceTarget.value);
+    event.preventDefault()
+    navigator.clipboard.writeText(this.sourceTarget.value)
   }
 }
