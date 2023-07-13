0705e8f9c32061bfb8e3151c373807fa05616708
le.tan.thanh <le.tan.thanh@sun-asterisk.com>
2023-07-13 21:05:25 +0700

    Setting Defaults

--- src/controllers/slideshow_controller.js
+++ src/controllers/slideshow_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["slide"]
-  static values = { index: Number }
+  static values = { index: { type: Number, default: 2 }, effect: { type: String, default: "kenburns" } }
 
   // initialize() {
   //   this.showCurrentSlide()
