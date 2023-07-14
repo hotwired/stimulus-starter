f9599ac7e49d88a14cfc50b58ed9ad9a379dc942
le.tan.thanh <le.tan.thanh@sun-asterisk.com>
2023-07-14 15:23:05 +0700

    Refreshing Automatically With a Timer

--- src/controllers/content_loader_controller.js
+++ src/controllers/content_loader_controller.js
@@ -1,13 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static values = { url: String }
+  static values = { url: String, refreshInterval: Number }
 
   connect() {
     this.load()
+    if (this.hasRefreshIntervalValue) {
+      this.startRefreshing()
+    }
   }
 
   // private
+
+  startRefreshing() {
+    setInterval(() => {
+      this.load()
+    }, this.refreshIntervalValue)
+  }
 
   load() {
     fetch(this.urlValue)
